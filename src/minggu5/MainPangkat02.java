871cd23438b0c1fb90647a3fb3ea59e504eee44e
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-03-11 04:03:24 +0700

    Pangkat Main

---
+++ src/minggu5/MainPangkat02.java
@@ -0,0 +1,30 @@
+package minggu5;
+
+import java.util.Scanner;
+
+public class MainPangkat02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen : ");
+        int elemen = input.nextInt();
+
+        Pangkat02[] png = new Pangkat02[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-" + (i + 1) + " : ");
+            int basis = input.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + " : ");
+            int pangkat = input.nextInt();
+            png[i] = new Pangkat02(basis, pangkat);
+        }
+
+        System.out.println("HASIL PANGKAT DENGAN BRUTE FORCE");
+        for (Pangkat02 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + " : " + p.pangkatBF(p.nilai, p.pangkat));
+        }
+
+        System.out.println("HASIL PANGKAT DENGAN DIVIDE CONQUER");
+        for (Pangkat02 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + " : " + p.pangkatDC(p.nilai, p.pangkat));
+        }
+    }
+}
